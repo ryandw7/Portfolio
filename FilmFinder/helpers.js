7b47d6bdb4f81e3be44e0d6dc8b93bab8fd41287
ryandw7 <134408930+ryandw7@users.noreply.github.com>
2024-01-19 02:44:25 +0000

    Updated Movie FilmFinder

--- FilmFinder/helpers.js
+++ FilmFinder/helpers.js
@@ -1,7 +1,6 @@
 // Populate dropdown menu with all the available genres
-const populateGenreDropdown = (genres) => {
-    const select = document.getElementById('genres')
-
+export const populateGenreDropdown = (genres) => {
+    const select = document.getElementById('genres');
     for (const genre of genres) {
         let option = document.createElement("option");
         option.value = genre.id;
@@ -11,19 +10,19 @@ const populateGenreDropdown = (genres) => {
 };
 
 // Returns the current genre selection from the dropdown menu
-const getSelectedGenre = () => {
+export const getSelectedGenre = () => {
     const selectedGenre = document.getElementById('genres').value;
     return selectedGenre;
 };
 
 // Displays the like and dislike buttons on the page
-const showBtns = () => {
+export const showBtns = () => {
     const btnDiv = document.getElementById('likeOrDislikeBtns');
     btnDiv.removeAttribute('hidden');
 };
 
 // Clear the current movie from the screen
-const clearCurrentMovie = () => {
+export const clearCurrentMovie = () => {
     const moviePosterDiv = document.getElementById('moviePoster');
     const movieTextDiv = document.getElementById('movieText');
     moviePosterDiv.innerHTML = '';
@@ -31,19 +30,19 @@ const clearCurrentMovie = () => {
 }
 
 // After liking a movie, clears the current movie from the screen and gets another random movie
-const likeMovie = () => {
+export const likeMovie = () => {
     clearCurrentMovie();
     showRandomMovie();
 };
 
 // After disliking a movie, clears the current movie from the screen and gets another random movie
-const dislikeMovie = () => {
+export const dislikeMovie = () => {
     clearCurrentMovie();
     showRandomMovie();
 };
 
 // Create HTML for movie poster
-const createMoviePoster = (posterPath) => {
+export const createMoviePoster = (posterPath) => {
     const moviePosterUrl = `https://image.tmdb.org/t/p/original/${posterPath}`;
 
     const posterImg = document.createElement('img');
@@ -54,7 +53,7 @@ const createMoviePoster = (posterPath) => {
 };
 
 // Create HTML for movie title
-const createMovieTitle = (title) => {
+export const createMovieTitle = (title) => {
     const titleHeader = document.createElement('h1');
     titleHeader.setAttribute('id', 'movieTitle');
     titleHeader.innerHTML = title;
@@ -63,7 +62,7 @@ const createMovieTitle = (title) => {
 };
 
 // Create HTML for movie overview
-const createMovieOverview = (overview) => {
+export const createMovieOverview = (overview) => {
     const overviewParagraph = document.createElement('p');
     overviewParagraph.setAttribute('id', 'movieOverview');
     overviewParagraph.innerHTML = overview;
@@ -72,14 +71,14 @@ const createMovieOverview = (overview) => {
 };
 
 // Returns a random movie from the first page of movies
-const getRandomMovie = (movies) => {
+export const getRandomMovie = (movies) => {
     const randomIndex = Math.floor(Math.random() * movies.length);
     const randomMovie = movies[randomIndex];
     return randomMovie;
 };
 
 // Uses the DOM to create HTML to display the movie
-const displayMovie = (movieInfo) => {
+export const displayMovie = (movieInfo) => {
     const moviePosterDiv = document.getElementById('moviePoster');
     const movieTextDiv = document.getElementById('movieText');
     const likeBtn = document.getElementById('likeBtn');
--- FilmFinder/script.js
+++ FilmFinder/script.js
@@ -1,41 +1,72 @@
+import { populateGenreDropdown, getSelectedGenre, clearCurrentMovie, getRandomMovie, displayMovie } from "./helpers.js";
 const tmdbKey = 'dba7ebef6d1fba77d7a6f91cfa59cac8';
 const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
 const getGenres = async () => {
- const genreRequestEndpoint = '/genre/movie/list';
- const requestParams = `?api_key=tmdbKey`;
- const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
- try {
- const response = await fetch(urlToFetch);
- if(response.ok){
-   const jsonResponse = await response.json();
-  console.log(jsonResponse);
-  const genres = jsonResponse.genres;
-  console.log(genres);
- }
- }catch(error){
-   console.log(error);
- }
+  const genreRequestEndpoint = '/genre/movie/list';
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      console.log('test')
+      const jsonResponse = await response.json();
+      const genres = jsonResponse.genres;
+      return genres;
+    }
+  } catch (error) {
+    console.log('bruh');
+  }
 };
 
-const getMovies = () => {
+const getMovies = async () => {
   const selectedGenre = getSelectedGenre();
+  console.log(selectedGenre)
+  const discoverMovieEndpoint = '/discover/movie';
+  const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+  const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+  console.log(urlToFetch)
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const movies = jsonResponse.results;
+      return movies;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const getMovieInfo = () => {
-
+const getMovieInfo = async (movie) => {
+  const movieId = movie.id;
+  const movieEndpoint = `/movie/${movieId}`;
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const movieInfo = await response.json();
+      return movieInfo;
+    }
+  } catch (error) {
+    console.log(error)
+  }
 };
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
-const showRandomMovie = () => {
+const showRandomMovie = async () => {
   const movieInfo = document.getElementById('movieInfo');
   if (movieInfo.childNodes.length > 0) {
     clearCurrentMovie();
   };
-
+  const movies = await getMovies();
+  const randomMovie = getRandomMovie(movies);
+  console.log(randomMovie.genre_ids)
+  const info = await getMovieInfo(randomMovie);
+  displayMovie(info);
 };
 
 getGenres().then(populateGenreDropdown);
 playBtn.onclick = showRandomMovie;
-
