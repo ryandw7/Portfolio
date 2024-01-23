9fed687af997f952b81fbd229208e767e3857a5d
ryandw7 <134408930+ryandw7@users.noreply.github.com>
2024-01-23 18:26:08 +0000

    i

--- Main/main.js
+++ Main/main.js
@@ -23,11 +23,14 @@ function eventListeners() {
 }
 
 eventListeners();
+
 const aboutMeP1 = document.getElementById("about-me-p1");
 const aboutMeP2 = document.getElementById("about-me-p2");
-let loadCompleted = false;
 const title1 = document.getElementById('title1');
 const title2 = document.getElementById('title2');
+
+
+
 function fadeIn(element) {
     element.style.opacity = 0;
     for (let i = 0; i < 200; i++) {
@@ -42,37 +45,42 @@ function fadeIn(element) {
 
 function stringTyper(string1, element1, string2, element2) {
     const char1 = string1.split('');
-    const char2 = string1.split('');
+    const char2 = string2.split('');
     const array1 = [];
     const array2 = [];
     for (let i = 0; i < (string1.length + string2.length); i++) {
         setTimeout(() => {
-            array1.pop()
-            array1.push(char1[i]);
-            array1.push('|')
-            element1.textContent = array1.join("");
+            if (i < string1.length) {
+                array1.pop()
+                array1.push(char1[i]);
+                array1.push('|')
+                element1.textContent = array1.join("");
+            }
         }, i * 25);
+
         setTimeout(() => {
-            array2.pop()
-            array2.push(char2[i]);
-            array2.push('|')
-            element2.textContent = array2.join("");
+            if (i < string2.length) {
+                array2.pop()
+                array2.push(char2[i]);
+                array2.push('|')
+                element2.textContent = array2.join("");
+            }
         }, (i + string1.length) * 25);
 
     }
+    setTimeout(() => {
+        array1.pop();
+        element1.textContent = array1.join("");
+        console.log('1')
+    }, 25 * string1.length);
+
+    setTimeout(() => {
+        array2.pop();
+        element2.textContent = array2.join("");
+        console.log(array1.join(""));
+    }, 25 * (string2.length + string1.length));
+
 }
-
-
-function typep1() {
-    stringTyper('test', aboutMe)
-}
-
-function typep2() {
-    stringTyper('bruh', aboutMe)
-}
-
-
-
 
 
 window.addEventListener("load", () => {
